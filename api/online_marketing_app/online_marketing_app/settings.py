3fea9f8dbe0ae730eb885ea4c4a9944898aefae8
stanley <stanley6453@gmail.com>
2023-09-21 13:32:23 +0100

    modified settings.py to load .env file

--- api/online_marketing_app/online_marketing_app/settings.py
+++ api/online_marketing_app/online_marketing_app/settings.py
@@ -19,12 +19,8 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-if getenv('ENVIRONMENT') == 'production':
-    load_dotenv(path.join(BASE_DIR.parent, '.env.production'))
-else:
-    load_dotenv()
-
+# load environment variables
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
